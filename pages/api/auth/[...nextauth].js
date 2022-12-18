ad181d11e1c78d49d2e0521867d0f263d50fdd73
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2022-12-17 19:37:56 -0800

    Add Google OAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,32 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ account, profile, user }) {
+      if (account.provider === 'google') {
+        // console.log({ profile }, 'signIn');
+        return (
+          profile.email_verified &&
+          (profile.email.endsWith('@ucla.edu') || profile.email.endsWith('.ucla.edu'))
+        );
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
 };
 
 export default NextAuth(authOptions);
