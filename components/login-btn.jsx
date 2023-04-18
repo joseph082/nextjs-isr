d8b2baad842c31f5aef732fb3d01dc3e8a1068b9
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-04-17 21:50:01 -0700

    Style login button

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -4,16 +4,25 @@ export default function Component() {
   const { data: session } = useSession();
   if (session) {
     return (
-      <>
+      <div>
         Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <button
+          onClick={() => signOut()}
+          className="bg-blue-400 rounded px-5 hover:bg-blue-700"
+          type="button"
+        >
+          Sign out
+        </button>
+      </div>
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      {/* Not signed in <br />
+      <button onClick={() => signIn()} className="bg-blue-400 rounded px-5 hover:bg-blue-700">
+        Sign in
+      </button> */}
+      Loading...
+    </div>
   );
 }
