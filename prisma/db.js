a9e5dd9d724c790fe804da8d792f08b4c21b7f5a
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-08-08 23:38:34 -0700

    Remove prisma edge client and route

--- prisma/db.js
+++ prisma/db.js
@@ -1,27 +1,10 @@
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from '@prisma/client';
-import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
 
-// ts
-const globalForPrisma = global;
-//  as unknown as {
-//   prisma: PrismaClient | undefined
-// }
+const globalForPrisma = globalThis;
 
-// $ npx prisma generate --data-proxy
-
-// export const prismaEdge =
-//   globalForPrisma.prismaEdge ??
-//   new PrismaClientEdge({
-//     log: ['query'],
-//   });
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClientEdge({
-    log: ['query'],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
-  // globalForPrisma.prismaEdge = prismaEdge;
 }
