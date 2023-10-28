30589ff9631d124c542f5912f7b3dae0e92a7b30
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-10-27 23:35:18 -0700

    Update login button

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -1,28 +1,20 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 
 export default function Component() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <div>
-        Signed in as {session.user.email} <br />
-        <button
-          onClick={() => signOut()}
-          className="bg-blue-400 rounded px-5 hover:bg-blue-700"
-          type="button"
-        >
-          Sign out
-        </button>
-      </div>
-    );
-  }
   return (
     <div>
-      {/* Not signed in <br />
-      <button onClick={() => signIn()} className="bg-blue-400 rounded px-5 hover:bg-blue-700">
-        Sign in
-      </button> */}
-      Loading...
+      {session ? `Signed in as ${session.user.email}` : 'Loading...'}
+      <br />
+      <button
+        onClick={() => signOut()}
+        className={`bg-blue-400 rounded px-5 hover:bg-blue-700${
+          !session ? ' aria-hidden invisible' : ''
+        }`}
+        type="button"
+      >
+        Sign out
+      </button>
     </div>
   );
 }
